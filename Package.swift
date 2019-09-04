40f577eafe9888ac312648a50c22ab8e0fdd8711
Sergey Kuleshov <svyatogor@gmail.com>
2019-09-04 14:53:39 +0300

    fix source path

--- Package.swift
+++ Package.swift
@@ -3,7 +3,7 @@
 import PackageDescription
 
 let package = Package(
-    name: "RxSwiftExt",
+    name: "RxAppState",
     platforms: [
         .iOS(.v8), .tvOS(.v9), .macOS(.v10_11)
     ],
@@ -16,8 +16,7 @@ let package = Package(
     targets: [
         .target(name: "RxAppState",
                 dependencies: ["RxSwift", "RxCocoa"],
-                path: "RxAppState",
-                sources: ["Pod/Classes"])
+                path: "Pod/Classes")
     ],
     swiftLanguageVersions: [.v4_2, .v5]
 )
